c193e03966b0a7cf32364a90a7fc7d1be7524a62
Patric Jansson <patricjansson@gmail.com>
2023-02-14 10:04:53 +0100

    $msg

--- update.sh
+++ update.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+echo "1. Hämtar det senaste från Github"
 git pull
+
+echo "2. Förbereder alla uppdaterade och borttagna filer för att skickas till Github"
 git add .
-read -p "Vad uppdaterade du (Max 100tkn): " msg
+
+read -p "Beskriv vad du uppdaterade (Max 100tkn): " msg
 
 if [ -z "${msg}" ]; then
-    msg = "Updated file(s)"
+    msg="Uppdaterade fil(er)"
 fi
+
+echo "3. Sparar alla filer på Github"
 git commit -m '$msg'
 
-
+echo "4. Klart!"
